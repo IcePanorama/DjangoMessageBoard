844d59dcc78ff119269e6c39a5b807ca6c9c6228
IcePanorama <thriftedroot@protonmail.com>
2024-07-17 11:41:53 -0400

    Refactored thread template using namespaces.

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "board"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("thread/<int:thread_id>/", views.thread, name="thread"),
