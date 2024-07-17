1f3c5c126ae252309a8be089c16df7be3aa0855d
IcePanorama <thriftedroot@protonmail.com>
2024-07-17 12:56:15 -0400

    Got basic thread submission set up.

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,7 @@ app_name = "board"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("submit/", views.submit, name="submit"),
     path("thread/<int:thread_id>/", views.thread, name="thread"),
     path("archive/", views.archive, name="archive"),
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .models import Thread
 
@@ -17,3 +18,28 @@ def thread(request, thread_id):
 
 def archive(request):
     return HttpResponse("You're looking at the archive.")
+
+
+def submit(request):
+    # Should handle cases where users simply navigate to the /submit/ url
+    if request.method != "POST":
+        return redirect("board:index")
+
+    # TODO: Handle blank posts
+    # should probably be done when the user tries to click the submit button
+    # rather than in here
+    if request.POST.get("post_text", "").strip() == "":
+        return redirect("board:index")
+
+    new_thread = Thread.objects.create(
+        date_posted=timezone.now(), post_text=request.POST.get("post_text"),
+    )
+
+    if request.POST.get("title", None):
+        new_thread.title = request.POST["title"]
+
+    if request.POST.get("name", None):
+        new_thread.name = request.POST["name"]
+
+    new_thread.save()
+    return redirect("board:thread", thread_id=new_thread.id)
